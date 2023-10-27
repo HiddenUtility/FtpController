5e290f62c12e34aa144c3d2198426df73208184f
Hiroki Uchimura <i.will.be.able.to.see.you@gmail.com>
2023-10-27 19:21:31 +0900

    create DirectoryMaker

--- src/DirectoryMaker.cs
+++ src/DirectoryMaker.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 
 namespace FtpController
 {
@@ -12,7 +13,28 @@ namespace FtpController
 
         public void MakeDirectory(string Dirname)
         {
-            
+            Uri newUri = new Uri(Info.RootUri, Dirname);
+            FtpWebRequest ftpWebRequest = (FtpWebRequest)WebRequest.Create(newUri);
+            ftpWebRequest.Credentials = new NetworkCredential(Info.Username, Info.Password);
+            ftpWebRequest.Method = WebRequestMethods.Ftp.MakeDirectory;
+            ftpWebRequest.KeepAlive = true;
+            ftpWebRequest.UseBinary = true;
+            ftpWebRequest.UsePassive = true;
+            ftpWebRequest.Proxy = null;
+
+            try
+            {
+                using(FtpWebResponse ftpWebResponse = (FtpWebResponse)ftpWebRequest.GetResponse())
+                {
+                    Console.WriteLine("{0}:{1}", ftpWebResponse.StatusCode, ftpWebResponse.StatusDescription);
+                }
+            }catch (WebException ex)
+            {
+                using(FtpWebResponse ftpWebResponse = (FtpWebResponse)ex.Response)
+                {
+                    Console.WriteLine($"{ftpWebResponse.StatusCode}:{ftpWebResponse.StatusDescription}");
+                }
+            }
         }
     }
 }
